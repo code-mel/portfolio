11952ded0ddb732dcf85bfb74dcfc99630fd9563
Melsm650 <wp.developerhelpers@gmail.com>
2018-04-04 22:37:47 -0700

    final test

--- app/app.js
+++ app/app.js
@@ -69,6 +69,9 @@ ajaxhttp.onreadystatechange = function () {
                 }
             ]
         });
+        $('.view-button').on('click', function () {
+            console.log('view project has been clicked');
+        });
 
     }
 };
